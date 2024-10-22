09129bd06db382f27a20c85461b52a9b24614ab8
Nikolaos Bakirtzis <nikolaos.bakirtzis@beg.utexas.edu>
2024-10-22 14:40:51 -0500

    Delete FSP_2.0_Legacy_Code directory

--- FSP_2.0_Legacy_Code/FSP_2.0/FSP2.0beta_MatlabFiles/callbackFunctions/callbacksavesess.m
+++
@@ -1,9 +0,0 @@
-function callbacksavesess(~,~,hDV)
-
-datsviz=hDV.data;
-% chkstring='DARREN PAIS URC 2016';  % changed in V0.99.1
-chkstring='Saved FSP Session'; 
-fname='FaultSlipPotential.mat';
-uisave({'datsviz','chkstring'},fname)
-
-end
